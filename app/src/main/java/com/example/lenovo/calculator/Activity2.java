3b2d32ff6948f709c23ab521ad53a27312c4d55c
krishnakantakandar <krishnakantakandar@gamil.com>
2020-12-31 18:42:46 +0530

    initial commit

---
+++ app/src/main/java/com/example/lenovo/calculator/Activity2.java
@@ -0,0 +1,29 @@
+package com.example.lenovo.calculator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class Activity2 extends AppCompatActivity {
+
+    EditText edtPercent;
+    EditText edtNumber;
+    Button btnResult1;
+    TextView txtResult1;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+
+       // edtPercent=(EditText)findViewById(R.id.edtPercent);
+       // edtNumber=(EditText)findViewById(R.id.edtNumber);
+        //txtResult1=(TextView)findViewById(R.id.txtResult1);
+        //btnResult1=(Button)findViewById(R.id.btnResult1);
+
+    }
+}
